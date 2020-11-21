722513ac48d4a165ce4797dd31d208d72561ceed
Hung Q. Truong <htq287@gmail.com>
2020-11-21 16:34:44 +0700

    * Creational - rename header file

--- creational/Creational/Creational.h
+++ creational/Creational/Creational.h
@@ -1,17 +1,17 @@
 //
-//  Corecreational.h
-//  Corecreational
+//  Creational.h
+//  Creational
 //
 //  Created by Hung Q. Truong on 11/19/20.
 //
 
 #import <Cocoa/Cocoa.h>
 
-//! Project version number for Corecreational.
-FOUNDATION_EXPORT double CorecreationalVersionNumber;
+//! Project version number for Creational.
+FOUNDATION_EXPORT double CreationalVersionNumber;
 
 //! Project version string for Corecreational.
-FOUNDATION_EXPORT const unsigned char CorecreationalVersionString[];
+FOUNDATION_EXPORT const unsigned char CreationalVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <Corecreational/PublicHeader.h>
 
